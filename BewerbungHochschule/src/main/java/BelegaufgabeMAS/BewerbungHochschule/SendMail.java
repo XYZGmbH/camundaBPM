0cd57404ce625d962e8e27cf8b57a1f4506f0e55
Felix <Felix@DESKTOP-ULK39NO.fritz.box>
2018-06-30 17:44:59 +0200

    SendMail JavaDoc

--- BewerbungHochschule/src/main/java/BelegaufgabeMAS/BewerbungHochschule/SendMail.java
+++ BewerbungHochschule/src/main/java/BelegaufgabeMAS/BewerbungHochschule/SendMail.java
@@ -13,63 +13,107 @@ import org.camunda.bpm.engine.runtime.MessageCorrelationResult;
 
 import io.FileReaderEmail;
 
-public class SendMail implements JavaDelegate{
+public class SendMail implements JavaDelegate {
 
+	/* (non-Javadoc)
+	 * Creates the right Email with the help of the setted type and Variables the user gave as input. 
+	 * If it's type is 'bestaetigung',
+	 * the 'matrikelnummer' will be read out of the execution and an Email with attachment will be send.
+	 * If not, either a 'zusage' or 'absage' Mail will be send.
+	 */
 	@Override
 	public void execute(DelegateExecution exec) throws Exception {
 		// TODO Auto-generated method stub
-		String 	anrede = (String) exec.getVariable("anrede"),
-				vorname = (String) exec.getVariable("vorname"),
-				nachname = (String) exec.getVariable("nachname"),
-				email = (String) exec.getVariable("email"),
-				type = (String) exec.getVariable("type"),
-				matrikelnummer = null;
-		
-		if(!(type.equalsIgnoreCase("zusage") || type.equalsIgnoreCase("absage"))) {
+		String anrede = (String) exec.getVariable("anrede"), vorname = (String) exec.getVariable("vorname"),
+				nachname = (String) exec.getVariable("nachname"), email = (String) exec.getVariable("email"),
+				type = (String) exec.getVariable("type"), matrikelnummer = null;
+
+		if (!(type.equalsIgnoreCase("zusage") || type.equalsIgnoreCase("absage"))) {
 			matrikelnummer = (String) exec.getVariable("matrikelnummer");
 		}
-		
+
 		this.sendEmail(email, type, anrede, vorname, nachname, matrikelnummer);
 
-		
 	}
-	
-	public String sendEmail(String email, String type, String anrede, String vorname, String nachname, String matrikelnummer) throws EmailException {
+
+	/**
+	 * sendEmail:
+	 * Sends an Email.
+	 * At first the right emailparts get loaded through the FileReaderEmail class.
+	 * After that, an Email for the right type is constructed and sended.
+	 * @param email
+	 * @param type
+	 * @param anrede
+	 * @param vorname
+	 * @param nachname
+	 * @param matrikelnummer
+	 * @return MessageId of Email
+	 * @throws EmailException
+	 *             an exception
+	 */
+	public void sendEmail(String email, String type, String anrede, String vorname, String nachname,
+			String matrikelnummer) throws EmailException {
 		FileReaderEmail fr = new FileReaderEmail();
 		HashMap<String, String> emailParts = fr.getEmailParts(type, anrede, vorname, nachname);
 		MultiPartEmail mail = null;
-		if(type.equalsIgnoreCase("zusage") || type.equalsIgnoreCase("absage")) {
+		if (type.equalsIgnoreCase("zusage") || type.equalsIgnoreCase("absage")) {
 			mail = this.createEmail(emailParts.get("body"), emailParts.get("subject"), email);
-		}else {
+		} else {
 			File f = new File("C:/Users/Felix/studentCard" + matrikelnummer + ".pdf");
 			EmailAttachment attach = new EmailAttachment();
 			attach.setPath(f.getPath());
 			attach.setDisposition(EmailAttachment.ATTACHMENT);
-			attach.setDescription("Logs");
+			attach.setDescription("Studentenausweis");
 			attach.setName(f.getName());
 			mail = this.createEmail(emailParts.get("body"), emailParts.get("subject"), email, attach);
 		}
-		
-		return mail.send();
+
+		mail.send();
 	}
-	
-	public MultiPartEmail createEmail (String mailtext, String subject, String toEmail, EmailAttachment f) throws EmailException {
+
+	/**
+	 * createEmail: creates an email and adds an attachment to it.
+	 * 
+	 * @param mailtext
+	 *            the mailtext
+	 * @param subject
+	 *            the subject of the mail
+	 * @param toEmail
+	 *            the mail receiver
+	 * @param f
+	 *            file, which is the attachment
+	 * @return email with attatchment
+	 * @throws EmailException
+	 *             an Exception
+	 */
+	public MultiPartEmail createEmail(String mailtext, String subject, String toEmail, EmailAttachment f)
+			throws EmailException {
 		MultiPartEmail email = this.createEmail(mailtext, subject, toEmail);
 		email.attach(f);
 		return email;
 	}
 
+	/**
+	 * createEmail: creates an email with a subject, mailtext, a receiver and the
+	 * needed technical details.
+	 * 
+	 * @param mailtext
+	 *            the mailtext
+	 * @param subject
+	 *            the subject of the mail
+	 * @param toEmail
+	 *            the mail receiver
+	 * @return a MultipartEmail which could be send
+	 * @throws EmailException
+	 *             an Exception
+	 */
 	public MultiPartEmail createEmail(String mailtext, String subject, String toEmail) throws EmailException {
-		//https://anleitungen.rz.htw-berlin.de/de/email/e-mail_programm/
-		
+		// https://anleitungen.rz.htw-berlin.de/de/email/e-mail_programm/
+
 		MultiPartEmail email = new MultiPartEmail();
 		email.setCharset("utf-8");
 		email.setSSL(true);
 		email.setSmtpPort(587);
-//		email.setHostName("mail.gmx.net");
-//		email.setAuthentication("XXXX@gmx.de", "XXXXXXX");
-//		email.addTo(toEmail);
-//		email.setFrom("XXXXXX@gmx.de");
 		email.setHostName("mail.htw-berlin.de");
 		//email.setAuthentication();
 		email.addTo(toEmail);
@@ -80,5 +124,4 @@ public class SendMail implements JavaDelegate{
 
 	}
 
-	
 }
